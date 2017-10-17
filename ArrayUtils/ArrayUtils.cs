61e3477c35042273f7eea88bd6118c34e7291d78
alexanderyakusik <alexander.yakusik@gmail.com>
2017-10-17 23:21:27 +0300

    Implemented MergeSort method and created tests for it.

--- ArrayUtils/ArrayUtils.cs
+++ ArrayUtils/ArrayUtils.cs
@@ -1,4 +1,6 @@
-﻿namespace ArrayUtils
+﻿using System;
+
+namespace ArrayUtils
 {
     public static class ArrayUtils
     {
@@ -9,7 +11,73 @@
 
         public static void MergeSort(int[] array)
         {
-            // TODO
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            RecursiveMerge(array, 0, array.Length);
+        }
+
+        // Sorts part of an array [leftIndex; rightIndex) recursively.
+        private static void RecursiveMerge(int[] array, int leftIndex, int rightIndex)
+        {
+            if (leftIndex + 1 >= rightIndex)
+            {
+                return;
+            }
+
+            int middleIndex = (rightIndex + leftIndex) / 2;
+
+            RecursiveMerge(array, leftIndex, middleIndex);
+            RecursiveMerge(array, middleIndex, rightIndex);
+
+            Merge(array, leftIndex, middleIndex, rightIndex);
+        }
+
+        // Merges two parts of an array.
+        // First part:  [leftIndex; middleIndex)
+        // Second part: [middleIndex; rightIndex)
+        private static void Merge(int[] array, int leftIndex, int middleIndex, int rightIndex)
+        {
+            int currentLeftIndex = leftIndex;
+            int currentRightIndex = middleIndex;
+            int currentBufferIndex = 0;
+
+            int[] buffer = new int[rightIndex - leftIndex];
+
+            while (currentLeftIndex < middleIndex && currentRightIndex < rightIndex)
+            {
+                if (array[currentLeftIndex] <= array[currentRightIndex])
+                {
+                    buffer[currentBufferIndex++] = array[currentLeftIndex++];
+                }
+                else
+                {
+                    buffer[currentBufferIndex++] = array[currentRightIndex++];
+                }
+            }
+
+            if (currentRightIndex == rightIndex)
+            {
+                while (currentLeftIndex < middleIndex)
+                {
+                    buffer[currentBufferIndex++] = array[currentLeftIndex++];
+                }
+            }
+            else
+            {
+                while (currentRightIndex < rightIndex)
+                {
+                    buffer[currentBufferIndex++] = array[currentRightIndex++];
+                }
+            }
+
+            currentBufferIndex = 0;
+            for (int i = leftIndex; i < rightIndex; i++)
+            {
+                array[i] = buffer[currentBufferIndex++];
+            }
         }
     }
 }
---
+++ ArrayUtilsTests/ArrayUtilsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ArrayUtils.Tests
+{
+    [TestClass]
+    public class ArrayUtilsTests
+    {
+        [TestMethod]
+        public void MergeSort_ValidArray_SortsCorrectly()
+        {
+            int[] actualArray   = new int[] { 7, 3, 5, 0, 1, 2, 6, 4 };
+            int[] expectedArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+            ArrayUtils.MergeSort(actualArray);
+
+            CollectionAssert.AreEqual(expectedArray, actualArray);
+        }
+
+        [TestMethod]
+        public void MergeSort_EmptyArray_SortsCorrectly()
+        {
+            int[] actualArray   = new int[0];
+            int[] expectedArray = new int[0];
+
+            ArrayUtils.MergeSort(actualArray);
+
+            CollectionAssert.AreEqual(expectedArray, actualArray);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MergeSort_NullPassed_ThrowsArgumentNullException()
+        {
+            int[] actualArray = null;
+
+            ArrayUtils.MergeSort(actualArray);
+        }
+    }
+}
